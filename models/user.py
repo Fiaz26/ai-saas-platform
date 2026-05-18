9b8178e52e8ef5f23b1279a5bd94223586d61490
Fiaz26 <fiaznns26@gmail.com>
2026-05-19 01:01:41 +0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,10 @@
-from app.extensions import db
+from extensions import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "users"
 
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-
-    role = db.Column(db.String(20), default="user")  # user / admin
-
-    credits = db.Column(db.Integer, default=100)
-
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    credits = db.Column(db.Integer, default=0)
+    is_admin = db.Column(db.Boolean, default=False)
