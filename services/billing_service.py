e006fc80b4eec95853ce22d31f01cb57a686baf7
Fiaz26 <fiaznns26@gmail.com>
2026-05-18 23:20:01 +0500

    Update billing_service.py

--- services/billing_service.py
+++ services/billing_service.py
@@ -1,1 +1,23 @@
+from app.models.user import User
+from app.models.payment import Payment
+from app.extensions import db
 
+def add_credits(user_id, amount):
+
+    user = User.query.get(user_id)
+    user.credits += amount
+
+    db.session.commit()
+
+
+def deduct_credits(user_id, amount=1):
+
+    user = User.query.get(user_id)
+
+    if user.credits < amount:
+        return False
+
+    user.credits -= amount
+    db.session.commit()
+
+    return True
