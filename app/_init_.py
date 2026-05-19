36993a0f976aff7b70d9ef7dff68cd17b98445f2
Fiaz26 <fiaznns26@gmail.com>
2026-05-19 21:08:11 +0500

    Move celery initialization inside create_app function
    Refactor celery initialization to be within create_app function.

--- app/_init_.py
+++ app/_init_.py
@@ -9,8 +9,21 @@ from app.api.v1.vendors import vendor_bp
 from app.api.v1.subscriptions import subscription_bp
 from app.api.v1.tasks import tasks_bp
 from app.api.v1.payments import payment_bp
-celery = make_celery(app)
+from app.celery_app import make_celery
 
+celery = None
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    global celery
+    celery = make_celery(app)
+
+    return app
 from app.middleware.logger import (
     before_request,
     after_request
