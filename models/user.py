ddd063df23eb5d7931150985399a8f4aeabb5533
Fiaz26 <fiaznns26@gmail.com>
2026-05-19 15:42:20 +0500

    Refactor User model and update default credits

--- models/user.py
+++ models/user.py
@@ -1,10 +1,8 @@
-from extensions import db
+from app.extensions import db
 
 class User(db.Model):
-    __tablename__ = "users"
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    credits = db.Column(db.Integer, default=0)
-    is_admin = db.Column(db.Boolean, default=False)
+
+    credits = db.Column(db.Integer, default=100)
