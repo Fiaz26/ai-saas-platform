74f842ca44439ef0d1dfd63a479fcc6424e41e73
Fiaz26 <fiaznns26@gmail.com>
2026-05-19 18:58:37 +0500

    Add Redis client initialization to extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from redis import Redis
 
 db = SQLAlchemy()
 jwt = JWTManager()
+
+redis_client = Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
