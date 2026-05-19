3028118ba701cdff3d8446dda81ed6ad011f0cf3
Fiaz26 <fiaznns26@gmail.com>
2026-05-19 13:00:53 +0500

    Load environment variables for configuration settings

--- app/config.py
+++ app/config.py
@@ -1,13 +1,13 @@
+from dotenv import load_dotenv
 import os
 
-class Config:
-    SECRET_KEY = "super-secret"
+load_dotenv()
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql://neondb_owner:npg_OPm5ADY7ZcfV@ep-quiet-frost-aoq7my5z-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require "
-    )
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://neondb_owner:npg_OPm5ADY7ZcfV@ep-quiet-frost-aoq7my5z-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require   ")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = "jwt-secret"
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
