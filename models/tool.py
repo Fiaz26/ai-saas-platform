12183d709edd74c30920ef0aa8fcb0fecd98e485
Fiaz26 <fiaznns26@gmail.com>
2026-05-19 13:30:15 +0500

    Update and rename tools.py to tool.py

--- models/tool.py
+++ models/tool.py
@@ -1,9 +1,10 @@
 from extensions import db
 
 class Tool(db.Model):
-    __tablename__ = "tools"
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+
+    name = db.Column(db.String(120), nullable=False)
+
     category = db.Column(db.String(120))
-    url = db.Column(db.String(255))
+
+    url = db.Column(db.String(500))
