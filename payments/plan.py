53473b135d9b2f6eee4242638414841a14b35086
Fiaz26 <fiaznns26@gmail.com>
2026-05-19 17:36:26 +0500

    Create plan.py

---
+++ payments/plan.py
@@ -0,0 +1,16 @@
+PLANS = {
+    "free": {
+        "credits": 50,
+        "price": 0
+    },
+
+    "pro": {
+        "credits": 1000,
+        "price": 2000
+    },
+
+    "enterprise": {
+        "credits": 10000,
+        "price": 15000
+    }
+}
