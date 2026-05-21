171f02007dc1c5016de5b10d494a7860630a77a6
Fiaz26 <fiaznns26@gmail.com>
2026-05-21 21:46:04 +0500

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from app import create_app
 app = create_app()
 app = create_app()
 celery = app.celery
+from routers.users import users_bp
 from app.routes.ai_routes import ai_bp
 app.register_blueprint(ai_bp)
+app.register_blueprint(users_bp)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7860, debug=True)
