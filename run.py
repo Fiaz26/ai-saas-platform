5c4a8781901de3cfcf7736f9f215b46a672ba1ae
Fiaz26 <fiaznns26@gmail.com>
2026-05-21 22:01:57 +0500

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ from routers.users import users_bp
 from app.routes.ai_routes import ai_bp
 app.register_blueprint(ai_bp)
 app.register_blueprint(users_bp)
+
 if __name__ == "__main__":
     print(app.url_map)
+    
     app.run(host="0.0.0.0", port=7860, debug=True)
