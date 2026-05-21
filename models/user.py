4734a7d8e2c97305e204d23c4c9556b0aeb84d61
Fiaz26 <fiaznns26@gmail.com>
2026-05-21 21:50:10 +0500

    Add Blueprint for user routes

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 from extensions import db
-
+from flask import Blueprint
+users_bp = Blueprint("users", __name__)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
     email = db.Column(db.String(120), unique=True)
 
     credits = db.Column(db.Integer, default=100)
