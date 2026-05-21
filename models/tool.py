3976058bdf3a1acb515acb8b273d891d427a603e
Fiaz26 <fiaznns26@gmail.com>
2026-05-21 23:32:37 +0500

    Refactor Tool model and add new fields

--- models/tool.py
+++ models/tool.py
@@ -1,10 +1,20 @@
-from extensions import db
+from app.extensions import db
+from datetime import datetime
 
 class Tool(db.Model):
+    __tablename__ = "tools"
+
     id = db.Column(db.Integer, primary_key=True)
 
-    name = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120))
+    category = db.Column(db.String(50))
 
-    category = db.Column(db.String(120))
+    endpoint = db.Column(db.String(255))
 
-    url = db.Column(db.String(500))
+    cost_per_use = db.Column(db.Integer, default=1)
+
+    vendor_id = db.Column(db.Integer)
+
+    is_active = db.Column(db.Boolean, default=True)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
