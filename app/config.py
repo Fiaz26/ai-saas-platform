c1fb44bc802486ec32e6bbeb353ba54cf3359fbf
Fiaz26 <fiaznns26@gmail.com>
2026-05-22 21:04:44 +0500

    Refactor database URI handling in Config class

--- app/config.py
+++ app/config.py
@@ -3,14 +3,15 @@ import os
 
 load_dotenv()
 
+import os
+
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
-    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://neondb_owner:npg_OPm5ADY7ZcfV@ep-quiet-frost-aoq7my5z-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require   ")
-
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "postgresql://neondb_owner:npg_OPm5ADY7ZcfV@ep-quiet-frost-aoq7my5z-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+        "sqlite:///app.db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
     
 JAZZCASH_MERCHANT_ID = "YOUR_MERCHANT_ID"
 JAZZCASH_INTEGRITY_SALT = "YOUR_SALT"
