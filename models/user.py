133fde7d0d87697eb0a19149846d3d3240d8420f
Fiaz26 <fiaznns26@gmail.com>
2026-05-22 19:58:13 +0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,18 +1,10 @@
 from app.extensions import db
-from datetime import datetime
 
 class User(db.Model):
-    __tablename__ = "users"
-
     id = db.Column(db.Integer, primary_key=True)
 
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
 
-    role = db.Column(db.String(20), default="user")  # user | vendor | admin
+    password = db.Column(db.String(255), nullable=False)
 
-    credits = db.Column(db.Integer, default=100)
-
-    is_active = db.Column(db.Boolean, default=True)
-
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    role = db.Column(db.String(50), default="user")
